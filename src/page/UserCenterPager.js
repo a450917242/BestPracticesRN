323c81de2c97e38cbffb02ffd795917508747d6d
REBOOTERS <foreverlove.zyl@163.com>
2017-08-25 11:35:38 +0800

    update view

--- src/page/UserCenterPager.js
+++ src/page/UserCenterPager.js
@@ -4,14 +4,15 @@
 import React, {Component} from 'react';
 
 import {
-    View, Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, ToastAndroid,
+    View, Text, StyleSheet, ScrollView, Image, Dimensions, TouchableOpacity, ToastAndroid, Animated,
 
 } from 'react-native';
 
-import PopupDialog, {DialogButton, DialogTitle, SlideAnimation,FadeAnimation} from 'react-native-popup-dialog';
+import PopupDialog, {DialogButton, DialogTitle, SlideAnimation, FadeAnimation} from 'react-native-popup-dialog';
 import ImagePicker from 'react-native-image-crop-picker';
 
 let windowWidth = Dimensions.get('window').width;
+let windowHeight = Dimensions.get('window').height;
 
 export default class UserCenterPager extends Component {
 
@@ -22,6 +23,8 @@ export default class UserCenterPager extends Component {
 
         this.state = {
             backgroundPhotoUri: 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=264358067,1297679371&fm=26&gp=0.jpg',
+            opacity: new Animated.Value(0),
+
         };
     }
 
@@ -46,16 +49,47 @@ export default class UserCenterPager extends Component {
             height: 400,
             cropping: true
         }).then(this.onPhotoResult)
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err);
             })
     };
 
 
+    _onContentScroll = (e) => {
+        // console.log(e.nativeEvent.contentOffset.y);
+
+        let percent = Math.abs(e.nativeEvent.contentOffset.y) / 200;
+
+        console.log(percent);
+
+        if (percent >= 1.0) {
+            percent = 1.0;
+        }
+
+        if (percent < 0) {
+            percent = 0;
+        }
+
+
+        this.setState(
+            {
+                opacity: percent,
+            }
+        )
+
+
+    };
+
     render() {
+
         return (
             <View style={styles.container}>
-                <ScrollView>
+
+
+                <ScrollView
+                    style={{height: windowHeight}} showsVerticalScrollIndicator={false}
+                    onScroll={this._onContentScroll}
+                >
                     <Image style={(styles.userInfobg)} source={{uri: this.state.backgroundPhotoUri}}>
                         <TouchableOpacity onPress={() => {
                             this.popupDialog.show();
@@ -64,7 +98,102 @@ export default class UserCenterPager extends Component {
                                    source={{uri: 'http://pic.jia360.com/ueditor/jsp/upload/201609/27/17601474946123480.png'}}/>
                         </TouchableOpacity>
                     </Image>
+
+
+                    <View style={{height: windowHeight * 3, width: windowWidth, backgroundColor: 'palegreen'}}>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                        <Text style={{color: 'white', fontSize: 53, margin: 20}}>
+                            A
+                        </Text>
+                    </View>
+
                 </ScrollView>
+
+                <Animated.View style={{
+                    backgroundColor: 'white',
+                    height: 56,
+                    opacity: this.state.opacity,
+                    width: windowWidth,
+                    alignItems: 'center',
+                    position: 'absolute',
+                    justifyContent: 'center'
+                }}>
+                    <Text style={{color: 'black', fontSize: 23, margin: 20}}>
+                        A
+                    </Text>
+
+                </Animated.View>
 
 
                 <PopupDialog
@@ -79,7 +208,7 @@ export default class UserCenterPager extends Component {
                     ref={(popupDialog) => {
                         this.popupDialog = popupDialog;
                     }}
-                    dialogAnimation={new FadeAnimation({toValue:1})}
+                    dialogAnimation={new FadeAnimation({toValue: 1})}
                 >
                     <View style={{alignItems: 'center', justifyContent: 'center'}}>
                         <TouchableOpacity onPress={this._openCamera}>
